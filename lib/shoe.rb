eb9267402750dcadb5b1c80441d2b50e329aab4d
Abbas Parpia <aparpia03@gmail.com>
2020-02-19 22:10:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition 
   attr_reader :brand
@@ -6,7 +8,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
-
+bidning.pry 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
