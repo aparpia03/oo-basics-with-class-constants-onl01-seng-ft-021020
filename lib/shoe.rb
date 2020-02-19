ff44d08215bd2503ee5052d156d8f5cad51fa8f1
Abbas Parpia <aparpia03@gmail.com>
2020-02-19 22:14:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRAND << @brand 
   end
-binding.pry 
+
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
