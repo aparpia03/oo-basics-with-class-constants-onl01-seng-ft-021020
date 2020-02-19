f9e9c7f0499943089027cc75eff71c2ae7010393
Abbas Parpia <aparpia03@gmail.com>
2020-02-19 21:50:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :unique
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, 
+  attr_reader :brand, :unique
   
   BRAND = []
 
@@ -13,5 +13,8 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
+  def unique
+    @unique 
   
 end 
